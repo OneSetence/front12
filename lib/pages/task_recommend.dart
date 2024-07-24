7169ae9bc8a54bab2a784b03254c3a4377618cff
Gu-Nayeon <list456@naver.com>
2024-07-24 20:28:45 +0900

    recommendtextadd

--- lib/pages/task_recommend.dart
+++ lib/pages/task_recommend.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../const/colors.dart';
 
-
 class TaskRecommend extends StatelessWidget {
   TaskRecommend({Key? key}) : super(key: key);
 
@@ -14,13 +13,42 @@ class TaskRecommend extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: white_01,
         title: Text(
-            '일정 순서 추천',
-            style: TextStyle(
-              fontSize: 17.0
-            ),
-        ),//title 지정
+          '일정 순서 추천',
+          style: TextStyle(
+            fontSize: 17.0,
+            color: Colors.black, // 텍스트 색상 설정
+          ),
+        ), //title 지정
+        elevation: 0, // AppBar의 그림자 제거
+        iconTheme: IconThemeData(color: Colors.black), // 아이콘 색상 설정
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0), // 원하는 패딩 값 설정
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+          children: [
+            Text(
+              '스킵이 예진 님의',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                // 텍스트 색상 설정
+              ),
+            ),
+            SizedBox(height:5.0),
+            Text(
+              '일정 순서를 추천해드렸어요.',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,// 텍스트 색상 설정
+              ),
+            ),
+            // 추가적으로 다른 위젯들도 여기에 넣을 수 있습니다.
+          ],
+        ),
+      ),
     );
   }
 }
